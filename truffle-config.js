dca32c34bfd7357d082bb11ebc353d7285f3c310
Udit Kumar <uditkumarussa@gmail.com>
2021-07-28 21:53:44 +0530

    integrated infura and truffle hd wallet

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,24 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config()
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
+const MNEMONIC = process.env.MNEMONIC;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
 module.exports = {
   networks: {
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(MNEMONIC,
+          `https://ropsten.infura.io/v3/${INFURA_API_KEY}`
+        )
+      },
+      gasPrice: 5500000,
+      network_id: 3,
+      confirmations: 2,
+      timeOfBlock: 200,
+      skipDryRun: true,
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
